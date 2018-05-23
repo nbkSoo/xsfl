669fa96a6ee76c2b077687f9d14561a05d245c7a
ywtg <470474080@qq.com>
2018-05-23 13:20:54 +0800

    12

--- js/index.js
+++ js/index.js
@@ -5,15 +5,7 @@ $(function() {
     Index.prototype = {
         constructor:Index,
         init:function(){
-            this.drawAxisLine();
         },
-        drawAxisLine:function(){
-            console.log();
-            if ($('.dataList:last-child').hasClass('waitingAdopt')) {
-                $('.dataList:last-child').children('.lines').css('height','0.8rem');
-            }
-        }
-
     }
     var index = new Index();
 })
